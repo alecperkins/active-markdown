58f43922428e34b4470211b90a63b712fece8383
Radivarig <reslav.hollos@gmail.com>
2015-06-16 19:29:10 +0200

    (add) coffee-loader and require resolving

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,13 @@ module.exports = {
         loaders: [
             { test: /\.jsx?$/, loaders: ['babel-loader?stage=1'], exclude: /node_modules/ }
           , { test: /\.css$/, loader: "style-loader!css-loader!postcss-loader" }
+          , { test: /\.coffee$/, loader: "coffee-loader" }
+          , { test: /\.(coffee\.md|litcoffee)$/, loader: "coffee-loader?literate" }
         ]
     }
+  , resolve: {
+        extensions: ['.css', '', '.jsx', '.js', '.coffee']
+    }
+
   , postcss: [ require('autoprefixer-core') ]
 }
